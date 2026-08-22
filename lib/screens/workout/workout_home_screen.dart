20c1f7b49b15231557327608193c94c21d8f01f1
RafaelGoulartB <rafagoulartb@gmail.com>
2026-08-22 09:55:56 -0300

    fix(workout): adjust child aspect ratio for grid layout in workout home screen

--- lib/screens/workout/workout_home_screen.dart
+++ lib/screens/workout/workout_home_screen.dart
@@ -685,7 +685,7 @@ class _WorkoutHomeScreenState extends State<WorkoutHomeScreen> {
           padding: EdgeInsets.zero,
           gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
             crossAxisCount: crossAxisCount,
-            childAspectRatio: 1.85,
+            childAspectRatio: 1.65,
             mainAxisSpacing: 0,
             crossAxisSpacing: 0,
           ),
